477b54d7d2d3c3376d75d0bdc39bfdf7ce037b58
East <micahlew@gmail.com>
2017-12-16 15:30:04 +0900

    We forgot some stuff the first time

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ app.get("/:date", (req, res) => {
 	res.json(time)
 })
 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('listening on port 3000'))
